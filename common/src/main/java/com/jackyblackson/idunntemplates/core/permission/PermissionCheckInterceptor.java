5d030535fbf58308152e1a42036511adc9d033b3
JackyBlackson <15510069979@163.com>
2026-02-03 20:01:10 +0800

    feat(common): permission interceptor

---
+++ common/src/main/java/com/jackyblackson/idunntemplates/core/permission/PermissionCheckInterceptor.java
@@ -0,0 +1,31 @@
+package com.jackyblackson.idunntemplates.core.permission;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+public class PermissionCheckInterceptor {
+
+    public static Boolean checkPermission(UUID uuid, String userName, String perm) {
+        String name = userName.toLowerCase();
+        String permission = perm.toLowerCase();
+        // create to self namespace
+        if(permission.startsWith(PermissionNames.Templates.createPersonal)) {
+            return true;
+        }
+        // create to self namespace
+        if(permission.startsWith(PermissionNames.Templates.createInPath$R + ".users." + name)) {
+            return true;
+        }
+        // commit to self namespace
+        if(permission.startsWith(PermissionNames.Templates.commitToPath$R + ".users." + name)) {
+            return true;
+        }
+        // use in self namespace
+        if(permission.startsWith(PermissionNames.Templates.usePath$R + ".users." + name)) {
+            return true;
+        }
+
+        return null;
+    }
+}
